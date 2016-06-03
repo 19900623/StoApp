bb89e5ab4d4ad9c4be53d6f6c2b38c91543c9733
kaoleba@126.com <kaoleba@126.com>
2016-06-03 17:25:18 +0800

    修改物料申请时间限制

--- js/page.js
+++ js/page.js
@@ -130,5 +130,21 @@
 			}, 0);
 		}, false);
 	};
+	
+	owner.toPerformance = function() {
+		var nwaiting = plus.nativeUI.showWaiting();
+		var webviewShow = plus.webview.create(
+			'report-performance.html',
+			'report-perfermance',
+			'', {}
+		); //后台创建webview并打开show.html
+		webviewShow.addEventListener("loaded", function() { //注册新webview的载入完成事件
+			setTimeout(function() {
+				nwaiting.close();
+				//新webview的载入完毕后关闭等待框
+				webviewShow.show("pop-in"); //把新webview窗体显示出来，显示动画效果为速度150毫秒的右侧移入动画
+			}, 0);
+		}, false);
+	};
 
 }(window.mui, window.page = {}));
