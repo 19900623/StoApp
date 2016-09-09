5bcc1d026aa41db7ab2b3345213f5ab40a16b5e4
kaoleba@126.com <kaoleba@126.com>
2016-09-09 16:06:46 +0800

    增加业务报表

--- js/page.js
+++ js/page.js
@@ -2,7 +2,25 @@
  * 登录界面JS
  **/
 (function($, owner) {
-
+	//业务部取件报表
+	//李凯辉
+	//2016.09.05
+	owner.toWaybillPickUp = function() {
+		var nwaiting = plus.nativeUI.showWaiting();
+		var webviewShow = plus.webview.create(
+			'report-waybillpickup-business.html',
+			'report-waybillpickup-business',
+			'', {}
+		); //后台创建webview并打开show.html
+		webviewShow.addEventListener("loaded", function() { //注册新webview的载入完成事件
+			setTimeout(function() {
+				nwaiting.close();
+				//新webview的载入完毕后关闭等待框
+				webviewShow.show("pop-in"); //把新webview窗体显示出来，显示动画效果为速度150毫秒的右侧移入动画
+			}, 0);
+		}, false);
+	};
+	
 	owner.toPickup = function() {
 		var nwaiting = plus.nativeUI.showWaiting();
 		var webviewShow = plus.webview.create(
