1e5124a5d9de0421fecbc2a145010a603d545ff5
Admir <admir_l@163.com>
2016-09-28 13:56:50 +0800

    新增部门流失客户报表 和 进港违规明细报表

--- js/page.js
+++ js/page.js
@@ -2,6 +2,40 @@
  * 登录界面JS
  **/
 (function($, owner) {
+	//进港违规明细报表
+	//李凯辉 2016.09.27
+	owner.toWrongDetail = function() {
+		var nwaiting = plus.nativeUI.showWaiting();
+		var webviewShow = plus.webview.create(
+			'report-wrong-detail.html',
+			'report-wrong-detail',
+			'', {}
+		); //后台创建webview并打开show.html
+		webviewShow.addEventListener("loaded", function() { //注册新webview的载入完成事件
+			setTimeout(function() {
+				nwaiting.close();
+				//新webview的载入完毕后关闭等待框
+				webviewShow.show("pop-in"); //把新webview窗体显示出来，显示动画效果为速度150毫秒的右侧移入动画
+			}, 0);
+		}, false);
+	};
+	//部门客户流失报表
+	//李凯辉 2016.09.26
+	owner.toCustomGroup = function() {
+		var nwaiting = plus.nativeUI.showWaiting();
+		var webviewShow = plus.webview.create(
+			'report-custom-dept.html',
+			'report-custom-dept',
+			'', {}
+		); //后台创建webview并打开show.html
+		webviewShow.addEventListener("loaded", function() { //注册新webview的载入完成事件
+			setTimeout(function() {
+				nwaiting.close();
+				//新webview的载入完毕后关闭等待框
+				webviewShow.show("pop-in"); //把新webview窗体显示出来，显示动画效果为速度150毫秒的右侧移入动画
+			}, 0);
+		}, false);
+	};
 	//业务部取件报表
 	//李凯辉
 	//2016.09.05
